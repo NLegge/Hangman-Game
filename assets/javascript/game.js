e850ffc05368adc45d5f13680b5f816f39002513
Your NameNLegge <little.legge@gmail.com>
2017-08-02 16:32:55 -0700

    second commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,48 @@
+window.onload = function () {
+
+var answers = ["s", "c", "h", "w", "i", "f", "t", "y"];
 //Game starts with a score of 0
-var wins = 0;
+var numWins = 0;
 //Game starts with 9 guesses
-var guesses = 9;
+var numGuesses = 9;
 //Variable to store letters guessed
-var letters;
-//variable to list answers
-var answers = {
-	schwifty: ["s", "c", "h", "w", "i", "f", "t", "y"],
-	moonman: ["m", "o", "o", "n", "m", "a", "n"],
-	
-}
+var letterGuess = "";
+//variable to list good guesses
+var goodGuess = "";
+
+
+var list = document.getElementsByTagName("li");
+//list.style.display === 'none';
+
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+      document.onkeyup = function(event) {
+        // Captures the key press, converts it to lowercase, and saves it to a variable.
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        letterGuess = userGuess + letterGuess;
+		letters.innerHTML = letterGuess;
+		numGuesses--;
+		guesses.innerHTML = numGuesses;
+
+      for (var i=0; i< answers.length; i++) {
+        if (userGuess === answers[i]) {
+        	goodGuess = userGuess + goodGuess;
+			word.innerHTML = goodGuess;
+        } 
+		else {
+
+		} 
+		}
+
+
+      };
+
+
+/*
 
 //function to update the wins
 function updateScore() {
         document.querySelector("#wins").innerHTML = wins;
 }
-
 
 //if letter is part of answer, populate #word & #letters and decrease #guesses by 1
 if () {
@@ -33,5 +60,9 @@ if () {
 }
 // if # of guesses reaches 0 and word not guessed, game over?
 else if {
+
+}
+*/
+
 
 }
